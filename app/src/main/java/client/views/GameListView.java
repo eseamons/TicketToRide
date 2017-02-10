2f9b7ff9df911ee449ab97c8c42c9f45b51b2764
Bexredd <r.redd94@gmail.com>
2017-02-10 15:57:50 -0700

    Started writing up the Presenter/View Interfaces and Classes for The game list view

--- app/src/main/java/client/views/GameListView.java
+++ app/src/main/java/client/views/GameListView.java
@@ -1,24 +1,63 @@
 package client.views;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.erics.tickettoride.R;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 
 import client.interfaces.IGameListView;
 import shared.model_classes.Game;
 
-public class GameListView implements IGameListView {
+public class GameListView extends AppCompatActivity implements IGameListView  {
 
-    List<String> AvaliableGames;
-    String GameName;
-    int NumPlayers;
-    Button JoinGame;
-    Button CreateGame;
+    List<String> avaliableGames;
+    String gameName;
+    int numPlayers;
+    Button joinGame;
+    Button createGame;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_list);
+
+        EditText gameNameText = (EditText)findViewById(R.id.gameName);
+        gameNameText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after)
+            {gameName = s.toString();}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count)
+            {gameName = s.toString();}
+
+            @Override
+            public void afterTextChanged(Editable s)
+            {gameName = s.toString();}
+        });
+
+
+
+
+    }
+
+
+
+
 
 
     @Override
     public String getGameName() {
-        return null;
+        return gameName;
     }
 
     @Override
@@ -30,8 +69,6 @@ public class GameListView implements IGameListView {
     public Game getSelectedGame() {
         return null;
     }
-
-
 
 
 }
