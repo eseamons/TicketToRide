d7ad46c12d6e4b8a53bfcfbbf9d3e81d1ad4cfea
Eric Seamons <ericseamons90@gmail.com>
2017-02-11 12:40:35 -0700

    Finished implementing all functions in ServerFacade. ServerModel now implements IServer and has same methods as ServerFacade. Register and Login functions are implemented in ServerModel. Working on CreateGame function

--- app/src/main/java/server/ServerFacade.java
+++ app/src/main/java/server/ServerFacade.java
@@ -1,18 +1,16 @@
 package server;
 
 import java.util.List;
-import java.util.UUID;
 
 import shared.ColorNum;
 import shared.interfaces.ICommand;
 import shared.interfaces.IServer;
 import shared.model_classes.Account;
 import shared.model_classes.GameLobby;
-import shared.model_classes.Player;
 
 public class ServerFacade implements IServer{
 
-    public static ServerFacade SINGLETON = null;
+    private static ServerFacade SINGLETON = null;
 
     public static ServerFacade getInstance()
     {
@@ -25,96 +23,55 @@ public class ServerFacade implements IServer{
     @Override
     public Account Login(String name, String pass) {
         ServerModel serverModel = ServerModel.getInstance();
-        List<Account> accountList = serverModel.getAccounts();
-        Account returnAccount = null;
-
-        //set returnAccount if both username and password match an account
-        for (Account account : accountList) {
-            if(account.getPassword() == pass && account.getUsername() == name) {
-                returnAccount = account;
-            }
-        }
-
-        return returnAccount;
+        return serverModel.Login(name, pass);
     }
 
     @Override
     public boolean Register(String name, String pass) {
         ServerModel serverModel = ServerModel.getInstance();
-        List<Account> accountList = serverModel.getAccounts();
-        boolean accountExists = false;
-        boolean isRegisterSuccessful = false;
-
-        //Check if accounts with same username already exists
-        for (Account account : accountList) {
-            if(account.getUsername() == name) {
-                accountExists = true;
-            }
-        }
-
-        if (accountExists == false) {
-            Account newAccount = new Account();
-            newAccount.setUsername(name);
-            newAccount.setPassword(pass);
-
-            //create authentication code
-            String uuid = UUID.randomUUID().toString();
-            newAccount.setAuthentication(uuid);
-        }
-
-        return isRegisterSuccessful;
+        return serverModel.Register(name, pass);
     }
 
     @Override
     public List<GameLobby> getServerGameList(String auth) {
         ServerModel serverModel = ServerModel.getInstance();
-        List<Account> accountList = serverModel.getAccounts();
-        List<GameLobby> returnLobbyList = null;
-
-        //Checks for auth code in accounts. If valid auth code, sets returnLobbyList
-        for (Account account : accountList) {
-            if(account.getAuthentication() == auth) {
-                returnLobbyList = serverModel.getLobbies();
-            }
-        }
-
-        return returnLobbyList;
+        return serverModel.getServerGameList(auth);
     }
 
     @Override
     public List<ICommand> getNewCommands(int ID, String auth) {
-        // TODO Auto-generated method stub
-        return null;
+        ServerModel serverModel = ServerModel.getInstance();
+        return serverModel.getNewCommands(ID, auth);
     }
 
     @Override
     public GameLobby CreateGame(String name, int players, String auth) {
-        // TODO Auto-generated method stub
-        return null;
+        ServerModel serverModel = ServerModel.getInstance();
+        return serverModel.CreateGame(name, players, auth);
     }
 
     @Override
     public GameLobby joinGame(int ID, String auth) {
-        // TODO Auto-generated method stub
-        return null;
+        ServerModel serverModel = ServerModel.getInstance();
+        return serverModel.joinGame(ID, auth);
     }
 
     @Override
     public boolean BeginGame(int ID, String auth) {
-        // TODO Auto-generated method stub
-        return false;
+        ServerModel serverModel = ServerModel.getInstance();
+        return serverModel.BeginGame(ID, auth);
     }
 
     @Override
     public boolean setPlayerColor(ColorNum color, String auth) {
-        // TODO Auto-generated method stub
-        return false;
+        ServerModel serverModel = ServerModel.getInstance();
+        return serverModel.setPlayerColor(color, auth);
     }
 
     @Override
     public boolean addComment(String message, String auth) {
-        // TODO Auto-generated method stub
-        return false;
+        ServerModel serverModel = ServerModel.getInstance();
+        return serverModel.addComment(message, auth);
     }
 
 }
--- app/src/main/java/server/ServerModel.java
+++ app/src/main/java/server/ServerModel.java
@@ -1,13 +1,17 @@
 package server;
 
 import java.util.List;
+import java.util.UUID;
 
+import shared.ColorNum;
 import shared.interfaces.ICommand;
 import shared.model_classes.Account;
 import shared.model_classes.Game;
 import shared.model_classes.GameLobby;
 
-public class ServerModel {
+import shared.interfaces.IServer;
+
+public class ServerModel implements IServer{
 
     private static ServerModel SINGLETON;
 
@@ -24,29 +28,102 @@ public class ServerModel {
     private List<GameLobby> lobbies;
     private List<Game> games;
     private List<ICommand> lobby_commands;
-    public List<Account> getAccounts() {
-        return accounts;
+
+    public Account Login(String name, String pass) {
+        Account returnAccount = null;
+
+        //set returnAccount if both username and password match an account
+        for (Account account : accounts) {
+            if(account.getPassword() == pass && account.getUsername() == name) {
+                returnAccount = account;
+            }
+        }
+
+        return returnAccount;
     }
-    public void setAccounts(List<Account> accounts) {
-        this.accounts = accounts;
+
+    public boolean Register(String name, String pass) {
+        boolean accountExists = false;
+        boolean isRegisterSuccessful = false;
+
+        //Check if accounts with same username already exists
+        for (Account account : accounts) {
+            if(account.getUsername() == name) {
+                accountExists = true;
+            }
+        }
+
+        if (accountExists == false) {
+            Account newAccount = new Account();
+            newAccount.setUsername(name);
+            newAccount.setPassword(pass);
+
+            //create authentication code
+            String uuid = UUID.randomUUID().toString();
+            newAccount.setAuthentication(uuid);
+            accounts.add(newAccount);
+        }
+
+        return isRegisterSuccessful;
+
     }
-    public List<GameLobby> getLobbies() {
-        return lobbies;
+
+    public List<GameLobby> getServerGameList(String auth) {
+        List<GameLobby> returnLobbyList = null;
+
+        //Checks for auth code in accounts. If valid auth code, sets returnLobbyList
+        for (Account account : accounts) {
+            if(account.getAuthentication() == auth) {
+                returnLobbyList = lobbies;
+            }
+        }
+
+        return returnLobbyList;
     }
-    public void setLobbies(List<GameLobby> lobbies) {
-        this.lobbies = lobbies;
+
+    @Override
+    public List<ICommand> getNewCommands(int ID, String auth) {
+        // TODO Auto-generated method stub
+        return null;
     }
-    public List<Game> getGames() {
-        return games;
+
+    @Override
+    public GameLobby CreateGame(String name, int players, String auth) {
+        GameLobby newGameLobby = null;
+        //Checks for auth code in accounts. If valid auth code, creates new Game lobby
+        for (Account account : accounts) {
+            if(account.getAuthentication() == auth) {
+                newGameLobby = new GameLobby();
+                newGameLobby.setName(name);
+
+            }
+        }
+
+        return null;
     }
-    public void setGames(List<Game> games) {
-        this.games = games;
+
+    @Override
+    public GameLobby joinGame(int ID, String auth) {
+        // TODO Auto-generated method stub
+        return null;
     }
-    public List<ICommand> getLobby_commands() {
-        return lobby_commands;
+
+    @Override
+    public boolean BeginGame(int ID, String auth) {
+        // TODO Auto-generated method stub
+        return false;
     }
-    public void setLobby_commands(List<ICommand> lobby_commands) {
-        this.lobby_commands = lobby_commands;
+
+    @Override
+    public boolean setPlayerColor(ColorNum color, String auth) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean addComment(String message, String auth) {
+        // TODO Auto-generated method stub
+        return false;
     }
 
 }
