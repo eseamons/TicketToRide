83f90231970eb9e46afe11985ef6cd9a6cbdd9f6
lkjames <thelancejames@gmail.com>
2017-02-15 21:38:09 -0700

    cleaning up

--- app/src/main/java/com/example/erics/tickettoride/MainActivity.java
+++ app/src/main/java/com/example/erics/tickettoride/MainActivity.java
@@ -3,11 +3,8 @@ package com.example.erics.tickettoride;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.EditText;
 
 import client.presenters.GameLobbyPresenter;
-import client.views.GameListView;
-import client.views.GameLobbyView;
 import client.views.LoginView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,15 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // start with this
-        //setContentView(R.layout.activity_login);
         startActivity(new Intent(MainActivity.this, LoginView.class));
         setContentView(R.layout.activity_login);
-
-//        //This area for testing GameLobbyView
-//        EditText msgText = (EditText) findViewById(R.id.msgEditText);
-//        //Create View first
-//        GameLobbyView lobbyView = new GameLobbyView(msgText);
 
         GameLobbyPresenter lobbyPresenter = new GameLobbyPresenter();
     }
