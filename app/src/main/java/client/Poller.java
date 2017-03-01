aa4647d73acd800fb6eda7db57bf8ad09387f4ed
Bexredd <r.redd94@gmail.com>
2017-03-01 15:55:22 -0700

    working on the poller

--- app/src/main/java/client/Poller.java
+++ app/src/main/java/client/Poller.java
@@ -10,25 +10,25 @@ import java.util.concurrent.TimeUnit;
 public class Poller
 {
     int times = 0;
-    public void runGetNonGameCommands()
-    {
-        ScheduledExecutorService scheduleTaskExecutor;
-
-        scheduleTaskExecutor= Executors.newScheduledThreadPool(5);
-
-            // This schedule a task to run every 1 second:
-        scheduleTaskExecutor.scheduleAtFixedRate(new Runnable()
-        {
-            public void run()
-            {
-                ClientFacade client = new ClientFacade();
-                client.getNewCommands();
-                //client.getServerGamesList(ClientModel.getInstance().getAuthorization());
-                Log.i("l", " T: " + times);
-                times++;
-            }
-        }, 3,2, TimeUnit.SECONDS);
-    }
+//    public void runGetNonGameCommands()
+//    {
+//        ScheduledExecutorService scheduleTaskExecutor;
+//
+//        scheduleTaskExecutor= Executors.newScheduledThreadPool(5);
+//
+//            // This schedule a task to run every 1 second:
+//        scheduleTaskExecutor.scheduleAtFixedRate(new Runnable()
+//        {
+//            public void run()
+//            {
+//                ClientFacade client = new ClientFacade();
+//                client.getNewCommands();
+//                //client.getServerGamesList(ClientModel.getInstance().getAuthorization());
+//                Log.i("l", " T: " + times);
+//                times++;
+//            }
+//        }, 3,2, TimeUnit.SECONDS);
+//    }
 
 
     public void runGetLobbyCommands()
