8d168dc7cb94e2ce4cfa37e6ccec9d41a15c6e8d
Bexredd <r.redd94@gmail.com>
2017-02-28 23:12:54 -0700

    Adding methods to the Train Card Deck (holds the Discard Deck)

--- app/src/main/java/shared/model_classes/TrainCardDeck.java
+++ app/src/main/java/shared/model_classes/TrainCardDeck.java
@@ -1,6 +1,8 @@
 package shared.model_classes;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import shared.ColorNum;
 
@@ -12,15 +14,62 @@ public class TrainCardDeck {
 
     private static TrainCardDeck instance = new TrainCardDeck();
 
-    private List<ColorNum> cardsInDeck;
+    private List<ColorNum> cardsInDeck = new ArrayList<>();
+    private List<ColorNum> discardDeck = new ArrayList<>();
 
-    private TrainCardDeck()
-    {
+    //TODO: should we add a discard deck in here or in another class?
+    //might be easier here so that you can keep the reshuffle in one place.
+
+    private TrainCardDeck() {
         //TODO:create colors in the Deck.
+        for(int i =0; i < 12; i++)
+        {
+            cardsInDeck.add(ColorNum.RED);
+            cardsInDeck.add(ColorNum.BLUE);
+            cardsInDeck.add(ColorNum.GREEN);
+            cardsInDeck.add(ColorNum.YELLOW);
+            cardsInDeck.add(ColorNum.BLACK);
+            //cardsInDeck.add(ColorNum.WHITE);
+            //cardsInDeck.add(ColorNum.ORANGE);
+            //cardsInDeck.add(ColorNum.PURPLE);
+        }
+        for(int j =0; j <14; j++)
+        {
+            //cardsInDeck.add(ColorNum.WILD);
+        }
+
+
     }
 
     public static TrainCardDeck getInstance() {
         return instance;
+    }
+
+    public ColorNum drawCard() {
+        int min = 0;
+        int max = cardsInDeck.size();
+        int randomIndex = (int) Math.random()*(max-min)+min;
+
+        ColorNum cardColor =cardsInDeck.get(randomIndex);
+        cardsInDeck.remove(randomIndex);
+
+        return cardColor;
+    }
+
+    public void addCardsToDiscard(List<ColorNum> cardColors) {
+
+        for(ColorNum color :cardColors )
+        {
+            discardDeck.add(color);
+        }
+    }
+
+    public void reShuffleDeck() {
+        for(ColorNum card: discardDeck)
+        {
+            cardsInDeck.add(card);
+        }
+        discardDeck = new ArrayList<>();
     }
 
 }
