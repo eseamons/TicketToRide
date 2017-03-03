eb8a2de2617d0809c6c64194a5f6a1d000ad2d3d
Bexredd <r.redd94@gmail.com>
2017-03-03 01:14:56 -0700

    Adding working Poller, Client Model NO LONGER calls update/ notify observers. Update will be called once every second from the Poller's onPostExecute method.

--- app/src/main/java/client/Poller.java
+++ app/src/main/java/client/Poller.java
@@ -7,13 +7,14 @@ import java.util.TimerTask;
 
 public class Poller
 {
+    Timer LobbyListTimer = new Timer();
     public void runGetLobbyCommands()
     {
 
         //this calls the Async task over and over again. The numbers 1,1 tell it how often to run
         //the higher the numbers the SLOWER it runs.
-        Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
+        //TODO: inorder to stop this when you join a game we will need to call
+        LobbyListTimer.schedule(new TimerTask() {
             @Override
             public void run() {
                 LobbyPolling l = new LobbyPolling();
@@ -22,11 +23,19 @@ public class Poller
         }, 1, 1);
     }
 
+    //I haven't tested this yet but this should stop the Timer from calling.
+    // We can use this to stop polling the Lobby List when we start a game.
+    //After that we could create another timer? Or something else if you guys have a better solution!
+    public void stopLobbyListTimer()
+    { LobbyListTimer.cancel();}
 
 
 
+    //Async task that has the code that was originally in the poller (Thank you Michael)
     public class LobbyPolling extends AsyncTask<Void, Void, Void>
     {
+
+        //this gets all the info from the server(MODEL DOES NOT UPDATE)
         @Override
         protected Void doInBackground(Void... params)
         {
@@ -36,6 +45,8 @@ public class Poller
             return null;
         }
 
+        //If there are any lobbies to show then the Model updates...
+        //Would be better if it had a way to update only if the sixe of the list increased.
         @Override
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
