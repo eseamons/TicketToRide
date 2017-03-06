9190be07f7dfe3ddecde7347268d905b4e5b38ce
lkjames <thelancejames@gmail.com>
2017-03-06 00:45:00 -0700

    deck and stats presenters

--- app/src/main/java/client/presenters/GameDeckPresenter.java
+++ app/src/main/java/client/presenters/GameDeckPresenter.java
@@ -1,8 +1,18 @@
 package client.presenters;
 
+import java.util.Observable;
+import java.util.Observer;
+
+import client.interfaces.IGameDeckPresenter;
+
 /**
  * Created by sirla on 3/3/2017.
  */
 
-public class GameDeckPresenter {
+public class GameDeckPresenter implements IGameDeckPresenter,Observer {
+
+    @Override
+    public void update(Observable observable, Object o) {
+
+    }
 }
