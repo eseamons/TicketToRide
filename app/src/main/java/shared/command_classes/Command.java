a870850e96bf8aeeffb3b318dea749d102f3f12c
Eric Seamons <ericseamons90@gmail.com>
2017-03-08 12:36:52 -0700

    added auth code to command class

--- app/src/main/java/shared/command_classes/Command.java
+++ app/src/main/java/shared/command_classes/Command.java
@@ -11,15 +11,19 @@ public class Command implements ICommand {
 
     protected Object info;
     protected int cmdID;
+    protected String auth;
 
     //setters
     public void setInfo(Object info) { this.info = info; }
     public void setCmdID(int cmdID) { this.cmdID = cmdID; }
+    public void setAuth(String auth) { this.auth = auth;}
+
     //getters
     public Object getInfo() {
         return info;
     }
     public int getCmdID() { return cmdID;}
+    public String getAuth() {return auth;}
 
     public void executeOnClient() {}
 
