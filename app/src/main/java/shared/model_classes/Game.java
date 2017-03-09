0d9252d97140c69032f52cb698f6a1b58835b616
mcoram <mcoram777@yahoo.com>
2017-03-08 18:12:05 -0700

    Added functionality to Client Facade methods

--- app/src/main/java/shared/model_classes/Game.java
+++ app/src/main/java/shared/model_classes/Game.java
@@ -21,7 +21,7 @@ public class Game {
     private DestinationCardsList destinationCardsList;
     private List<Command> commands;
     private List<String> comments = new ArrayList<>();
-    private int currentPlayer;
+    //private int currentPlayer;  don't use this, use playersList.something
 
 
     public Game(){}
@@ -33,16 +33,8 @@ public class Game {
         faceUpCards = new ArrayList<ColorNum>(5);
         destinationCardsList = new DestinationCardsList();
         routes = new RoutesList();
-        currentPlayer = 0;
     }
 
-    public int getCurrentPlayer() {
-        return currentPlayer;
-    }
-    public void setCurrentPlayer(int currentPlayer) {
-        this.currentPlayer = currentPlayer;
-    }
-    public void incrementCurrentPlayer() { this.currentPlayer++; }
 
     public RoutesList getRoutes(){return routes;}
     public int getGameID() {
